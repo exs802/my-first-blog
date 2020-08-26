b5228f8f56ce41cf003014e1e0a6e918711ddc8e
Eni Segun <exs802@student.bham.ac.uk>
2020-08-26 09:53:47 +0100

    unit test that checks home page is working

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class CVPageTest(TestCase):
+    def test_uses_home_page_template(self):
+        response = response = self.client.get('/')
+        self.assertTemplateUsed(response, 'blog/home_page.html')
