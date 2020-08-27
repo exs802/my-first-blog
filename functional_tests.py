1d8c8bfeafc66a7e008324877d0dbb766fb14a46
Eni Segun <exs802@student.bham.ac.uk>
2020-08-27 10:01:47 +0100

    functional test up and running - simple version

--- functional_tests.py
+++ functional_tests.py
@@ -21,5 +21,19 @@ class NewVisitorTest(unittest.TestCase):
         goToCV = self.browser.get('http://localhost:8000/mycv')
         self.assertIn('Eni Segun CV', self.browser.find_element_by_class_name('cv_name').text)
 
+        editbox = self.browser.find_element_by_id('id_edit_section')
+
+        editbox.click()
+        time.sleep(1)
+
+        inputbox = self.browser.find_element_by_class_name('post-form')
+        inputbox.send_keys('I have edited my CV')
+
+        savebtn = self.browser.find_element_by_id('save btn btn-default')
+        savebtn.click()
+        time.sleep(1)
+
+        self.assertIn('I have edited my CV', self.browser.find_element_by_class_name('cv_text'))
+
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')  
