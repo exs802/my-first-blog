36d236e3c481ee4cb880367de4a0a252b793f4de
Eni Segun <exs802@student.bham.ac.uk>
2020-08-30 05:31:36 +0100

    functional tests done in isolation and clean up after themselves - staticliveservertestcase used now

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,29 +1,31 @@
 from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-import time
-import unittest
 from blog.models import CVItem
+import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
+        CVItem.objects.create(title='Personal Info', text='email: exs802@student.bham.ac.uk')
+        CVItem.objects.create(title='Work Experience', text="JLP, Apple, Ocado")
+        CVItem.objects.create(title='Qualifications', text='GCSE')
+        CVItem.objects.create(title='Education', text="Townley Grammar")
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
-        editedSection = CVItem.objects.get(text='I have edited my CV')
-        editedSection.text = "email: exs802@studetn.bham.ac.uk"
-        editedSection.save()
         self.browser.quit()
 
     def test_can_edit_and_save_cv(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('Django Girls Blog', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text  
         self.assertIn('Django Girls Blog', header_text)
 
-        goToCV = self.browser.get('http://localhost:8000/mycv')
+        self.browser.get(self.live_server_url + '/mycv')
+        time.sleep(5)
         self.assertIn('Eni Segun CV', self.browser.find_element_by_class_name('cv_name').text)
 
         editbox = self.browser.find_element_by_id('id_edit_section')
@@ -43,17 +45,16 @@ class NewVisitorTest(unittest.TestCase):
 
         self.assertIn('I have edited my CV', self.browser.find_element_by_class_name('cv_text').text)
 
-class EditCVTest(unittest.TestCase):
+class EditCVTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
-        CVItem.objects.get(text='I have added a new section to my CV').delete()
         self.browser.quit()
 
-    def test_can_add_new_cv_section(self):
-        self.browser.get('http://localhost:8000/mycv')
+    def best_can_add_new_cv_section(self):
+        self.browser.get(self.live_server_url + '/mycv')
         self.assertIn('Eni Segun CV', self.browser.find_element_by_class_name('cv_name').text)
 
         addSection = self.browser.find_element_by_id('id_add_section')
