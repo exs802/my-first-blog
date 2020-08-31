d0ea975a31b4303e4027b694dca2c68c4df914d2
Eni Segun <exs802@student.bham.ac.uk>
2020-09-01 00:42:42 +0100

    functional tests now log in as an admin so edit buttons can be accessed

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.auth.models import User
+from django.test.client import Client
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from blog.models import CVItem
@@ -14,18 +16,32 @@ class NewVisitorTest(StaticLiveServerTestCase):
         CVItem.objects.create(title='Education', text="Townley Grammar")
         self.browser = webdriver.Firefox()
 
+        self.user = User.objects.create_superuser('testuser', 'exs802@student.bham.ac.uk', '12345')
+        self.assertEqual(User.objects.all().count(), 1)
+        
     def tearDown(self):
         self.browser.quit()
 
     def test_can_edit_and_save_cv(self):
+        
+
+        self.browser.get(self.live_server_url + '/admin/')
+        username_input = self.browser.find_element_by_name("username")
+        username_input.send_keys('testuser')
+        password_input = self.browser.find_element_by_name("password")
+        password_input.send_keys('12345')
+        self.browser.find_element_by_xpath('//input[@value="Log in"]').click()
+
         self.browser.get(self.live_server_url)
+
+        time.sleep(1)
 
         self.assertIn('Django Girls Blog', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text  
-        self.assertIn('Django Girls Blog', header_text)
+        self.assertIn('DJANGO GIRLS BLOG', header_text)
 
         self.browser.get(self.live_server_url + '/mycv')
-        self.assertIn('Eni Segun CV', self.browser.find_element_by_class_name('cv_name').text)
+        self.assertIn('ENIOLUWA SEGUN', self.browser.find_element_by_class_name('cv_name').text)
 
         editbox = self.browser.find_element_by_id('id_edit_section')
         editbox.click()
@@ -43,17 +59,29 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         self.assertIn('I have edited my CV', self.browser.find_element_by_class_name('cv_text').text)
 
-class EditCVTest(StaticLiveServerTestCase):
+class NewSectionTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+        
+        self.user = User.objects.create_superuser('testuser', 'exs802@student.bham.ac.uk', '12345')
+        self.assertEqual(User.objects.all().count(), 1)
+        
+   
     def tearDown(self):
         self.browser.quit()
 
     def test_can_add_new_cv_section(self):
+        self.browser.get(self.live_server_url + '/admin/')
+        username_input = self.browser.find_element_by_name("username")
+        username_input.send_keys('testuser')
+        password_input = self.browser.find_element_by_name("password")
+        password_input.send_keys('12345')
+        self.browser.find_element_by_xpath('//input[@value="Log in"]').click()
+
         self.browser.get(self.live_server_url + '/mycv')
-        self.assertIn('Eni Segun CV', self.browser.find_element_by_class_name('cv_name').text)
+        self.assertIn('ENIOLUWA SEGUN', self.browser.find_element_by_class_name('cv_name').text)
 
         addSection = self.browser.find_element_by_id('id_add_section')
 
